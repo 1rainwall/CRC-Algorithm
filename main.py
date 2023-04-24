33735e678d660996e5653d6b97d5e1f2f9038d86
Rain <89419093+1rainwall@users.noreply.github.com>
2023-04-24 00:20:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,55 @@ from tqdm import tqdm
 import time
 from termcolor import colored
 
-# Se solicita al usuario que ingrese un número decimal
-valor_digitado = int(input("Ingrese un número decimal: "))
 
-# Se convierte el valor digitado a binario
-valor_binario = bin(valor_digitado)[2:]
+def polinomio_a_coeficientes(polinomio):
+    coeficientes = []
+    for i in range(len(polinomio)):
+        if polinomio[i] == 'x':
+            if i == 0 or polinomio[i-1] == '+':
+                coeficientes.append(1)
+            elif polinomio[i-1] == '-':
+                coeficientes.append(-1)
+            else:
+                coeficientes.append(int(polinomio[i-1]))
+    grado = len(coeficientes) - 1
+    return coeficientes, grado
 
-# Se muestra en pantalla el valor digitado en binario
-print("Valor digitado en binario:", valor_binario)
 
-# Se define el polinomio generador en formato binario
+def evaluar_polinomio(coeficientes, x):
+    resultado = 0
+    for i in range(len(coeficientes)):
+        resultado += coeficientes[i] * (x ** i)
+    return resultado
+
+
+def decimal_a_binario(decimal, num_bits):
+    binario = bin(decimal)[2:]
+    if len(binario) < num_bits:
+        binario = '0' * (num_bits - len(binario)) + binario
+    return binario
+
+
+polinomio = input(
+    "Ingrese un polinomio de la forma 'ax^n + bx^(n-1) + ... + c': ")
+x = int(input("Ingrese el valor de x para evaluar el polinomio: "))
+coeficientes, grado = polinomio_a_coeficientes(polinomio)
+valor_digitado = evaluar_polinomio(coeficientes, x)
+valor_binario = decimal_a_binario(valor_digitado, grado + 1)
 polinomio_generador = [1, 1, 0, 1]
-
-# Se extiende el valor binario con ceros adicionales al final
 valor_extendido = valor_binario + "0" * (len(polinomio_generador) - 1)
 
-# Se muestra una barra de carga mientras se realiza la operación CRC
 with tqdm(total=len(valor_binario), desc="Procesando", bar_format="{l_bar}{bar} {n_fmt}/{total_fmt}", ncols=50) as pbar:
     for i in range(len(valor_binario)):
         if valor_extendido[i] == "1":
-            # Si el bit en la posición i es 1, se realiza la operación XOR entre los bits del polinomio generador y los bits del valor extendido
             for j in range(len(polinomio_generador)):
                 valor_extendido = valor_extendido[:i+j] + str(int(
-                    polinomio_generador[j] != int(valor_extendido[i+j]))) + valor_extendido[i+j+1:]
-        time.sleep(0.1)  # Simulamos un poco de procesamiento
+                    polinomio_generador[j] != int(valor_extendido[i+j])))+valor_extendido[i+j+1:]
         pbar.update(1)
+        time.sleep(0.1)
 
-# Se obtiene el resultado del algoritmo CRC
-resultado_crc = valor_extendido[-(len(polinomio_generador)-1):]
-
-# Se muestra en pantalla el valor digitado en binario y el resultado del algoritmo CRC en color
-print("Valor digitado en binario: ", valor_binario)
-print("Resultado del algoritmo CRC: ", colored(resultado_crc, "green"))
+valor_CRC = valor_extendido[-(len(polinomio_generador)-1):]
+mensaje_CRC = valor_binario + valor_CRC
+mensaje_CRC_C = mensaje_CRC[:len(valor_binario)] + \
+    colored(mensaje_CRC[len(valor_binario):], "green")
+print("Mensaje CRC:", mensaje_CRC_C)
